e97073dfab4e6efba37b95d1dd0e744c0b72aa94
Pushpesh Basak <85738088+PB-1906@users.noreply.github.com>
2022-08-20 09:26:24 +0530

    Time: 908 ms (15.03%), Space: 291.8 MB (11.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,29 +1,28 @@
 #include <bits/stdc++.h>
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-      int n=nums.size();
-      vector<vector<int>> dp(n,vector<int>(n+1,-1));
-      return lis(nums,n,0,-1,dp);
-      }
- int lis(vector<int> &nums, int n,  int ind, int prev, vector<vector<int>>& dp){
+    int lengthOfLIS(vector<int>& arr) {
+      int n=arr.size();
+      vector<vector<int>> dp(n+1,vector<int>(n+1,0));
     
-    // base condition
-    if(ind == n)
-        return 0;
-        
-    if(dp[ind][prev+1]!=-1)
-        return dp[ind][prev+1];
+    for(int ind = n-1; ind>=0; ind --){
+        for (int prev = ind-1; prev >=-1; prev --){
+            
+            int notTake = 0 + dp[ind+1][prev +1];
     
-    int notTake = 0 + lis(nums,n,ind+1,prev,dp);
+            int take = 0;
     
-    int take = 0;
+            if(prev == -1 || arr[ind] > arr[prev]){
+                
+                take = 1 + dp[ind+1][ind+1];
+            }
     
-    if(prev == -1 || nums[ind] > nums[prev]){
-        take = 1 + lis(nums,n,ind+1,ind,dp);
+            dp[ind][prev+1] = max(notTake,take);
+            
+        }
     }
     
-    return dp[ind][prev+1] = max(notTake,take);
-}
+    return dp[0][0];
+      }
     
 };
