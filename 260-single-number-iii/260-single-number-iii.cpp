b03632fcdd4e494b3756e3e0743df38318154342
Pushpesh Basak <85738088+PB-1906@users.noreply.github.com>
2022-08-29 22:21:28 +0530

    Time: 14 ms (58.26%), Space: 9.8 MB (99.69%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+          int xor1 = 0;
+        for(auto i : nums)
+            xor1=xor1^i;
+        
+        int cnt=0;
+        while(xor1)
+        {
+            if(xor1&1)
+                break;
+            else
+            {
+                cnt++;
+                xor1 = xor1>>1;
+            }
+        }
+        int xor2 = 0;
+        int xor3 =0 ;
+        
+        for(auto i :nums)
+        {
+            if(i&(1<<cnt))
+                xor2 = xor2 xor i;
+            else
+                xor3 =  xor3^i;
+        }
+            
+        return {xor2,xor3};
+    }
+};
