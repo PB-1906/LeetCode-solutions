eceee67d63c1217ce9406a6d9efff97263dee298
Pushpesh Basak <85738088+PB-1906@users.noreply.github.com>
2022-09-04 09:29:46 +0530

    Time: 27 ms (64.92%), Space: 16.5 MB (81.19%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-         int current = 0; 
+         int cur = 0; 
         int jump = 0;
-        int des = 0;
+        int dest = 0;
         for(int i =0;i<nums.size()-1;i++){
-            des = max(des,nums[i]+i);
-            if(i==current){
-                current = des;
+            dest = max(dest,nums[i]+i);
+            if(i==cur){
+                cur = dest;
                 jump++;
             }
         }
