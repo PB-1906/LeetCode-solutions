9b33de3287100368cb8333e332884f1d5651ce21
Pushpesh Basak <85738088+PB-1906@users.noreply.github.com>
2022-09-16 09:14:00 +0530

    Time: 857 ms (28.84%), Space: 119.9 MB (68.36%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solve(vector<int>& nums, vector<int>& multipliers, int l, int i, int n, int m){
+        if(i >= m) return 0;
+        if(dp[l][i] != INT_MIN) return dp[l][i];
+        
+        int leftPicked = solve(nums, multipliers, l + 1, i + 1, n, m) + nums[l] * multipliers[i];
+        int rightPicked = solve(nums, multipliers, l, i + 1, n, m) + nums[n - (i - l) - 1] * multipliers[i];
+        
+        return dp[l][i] = max(leftPicked, rightPicked);
+    }
+    
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int n = nums.size(), m = multipliers.size();
+        dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
+        return solve(nums, multipliers, 0, 0, n, m);
+    }
+};
