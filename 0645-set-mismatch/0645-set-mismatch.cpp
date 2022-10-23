9a80c7ef63162e01ce7b74a6509421a266ea4760
Pushpesh Basak <85738088+PB-1906@users.noreply.github.com>
2022-10-23 11:55:22 +0530

    Time: 78 ms (53.22%), Space: 22.3 MB (39.92%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+      int n = nums.size();
+      vector<int> count(n);
+      vector<int> ans(2);
+      for(int i = 0; i < n; i++) {
+            ans[1] ^= (i+1) ^ nums[i];
+            if (++count[nums[i]-1] == 2)
+                ans[0] = nums[i];
+        }
+        ans[1] ^= ans[0];
+        return ans;
+    }
+};
